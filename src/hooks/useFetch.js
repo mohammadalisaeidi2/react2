2c2e9f7b41bef4b560fdbaadaa8b90343ba18338
Mohammadali <mohammadalisaeidi@yahoo.com>
2022-11-13 18:08:36 +0330

    Add Error handler & loading page
    E57

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,17 +1,36 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
-export const useFetch = (url) => {
+export const useFetch = (url , _option) => {
+    //use useRef to wrap an object/array ("_option"in this case)
+    //which is a useEffect dependency
+    const option = useRef(_option)
+
     const [data, setData] = useState(null)
+    const [isPending, setIsPending] = useState(false)
+    const [error, setError] = useState(null)
 
     useEffect(() => {
+        console.log(option)
         const fetchData = async () =>{
-            const res = await fetch(url)
-            const json = await res.json()
+            setIsPending(true)
+            try {
+              const res = await fetch(url)
+              if(!res.ok){
+                throw new Error(res.statusText)
+              }
+              const json = await res.json()
+              setIsPending(false)
+              setData(json)
+              setError(null)
+            }catch (err) {
+                setIsPending(false)
+                setError('could nodt fetch data')
+                console.log(err.message)
+            }
 
-            setData(json)
         }
         fetchData()
 
-    } ,[url])
-     return { data:data }
+    } ,[url,option])
+     return { data:data,isPending, error }
 }
